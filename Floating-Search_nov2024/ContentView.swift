c9d81a3e642f5c74a7700a974da2630c80536da1
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-11-25 12:46:16 +0530

    Initial Commit

---
+++ Floating-Search_nov2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Floating-Search_nov2024
+//
+//  Created by mac on 25/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Floating-Search_nov2024/Floating_Search_nov2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Floating_Search_nov2024App.swift
+//  Floating-Search_nov2024
+//
+//  Created by mac on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Floating_Search_nov2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
